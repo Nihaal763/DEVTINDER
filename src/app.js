3f0197e8a0b90df9fe1302f56a531e7a18fa0cd2
Nihaal763 <nihaalkumar.cfort@gmail.com>
2026-01-23 13:10:39 +0530

    making changes in data dynamic

--- src/app.js
+++ src/app.js
@@ -16,26 +16,61 @@ connectDB().then(() => {
 
 app.use(express.json());
 
-app.post("/signup" , async (req , res)=> {
-    // const userObj = {
-    //     firstName : "Sachin",
-    //     lastName : "Tendulkar",
-    //     emailId : "Sachin@gmail.com",
-    //     password : "Sachin  @12345"
-    // }
-      const user = new User(req.body);
+// app.post("/signup" , async (req , res)=> {
+//     // const userObj = {
+//     //     firstName : "Sachin",
+//     //     lastName : "Tendulkar",
+//     //     emailId : "Sachin@gmail.com",
+//     //     password : "Sachin  @12345"
+//     // }
+//       const user = new User(req.body);
     
+//     try{
+//        const m = await user.save();
+//        res.send("User added successfully");
+//     }
+//     catch{      
+//        res.status(401).send("Error saving the usrer");
+//     }
+//     console.log(req.body);
+// })
+
+// app.get("/user" , async (req , res) => {
+
+//     try{
+//          const email = req.body.emailId;
+//          const user = await User.find({emailId : email});
+//          if(user.length === 0){
+//             res.status(400).send("cannot find a user with this details");
+//          }
+//          else{
+//             res.send(user);
+//          }     
+//     }
+//     catch{
+//         res.status(400).send("cannot find a user with this details");
+//     }
+   
+// })
+
+
+
+
+app.get("/feed" , async (req ,res)=> {
     try{
-       const m = await user.save();
-       res.send("User added successfully");
+        const user = await User.find({});
+        res.send(user);
+        if(user.length === 0){
+            res.send("No data of the users yet it starts from now");
+        }
+        else{
+            res.send(user);
+        }
     }
-    catch{      
-       res.status(401).send("Error saving the usrer");
+    catch{
+        res.status(400).send("cannot find a user with this details");   
     }
-    console.log(req.body);
-})
-
-
+});
 
 
 // Connect to the mongoose cluster
